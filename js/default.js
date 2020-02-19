cf77660fe50cf8abd539068959b8926fd26b84a7
Wouter Koopman <wouter.koopman@student.fontys.nl>
2020-02-19 17:56:35 +0100

    Added season check

--- js/default.js
+++ js/default.js
@@ -1,8 +1,13 @@
 window.onload = init;
 
 var curDate = new Date();
-var fDate = GetNextDayOfWeek(new Date(), 4, 8);
-var chDate = fDate.getTime();
+var fDate = new Date();
+var chDate;
+
+const se = ["winter", "spring", "summer", "autumn"];
+const origin = new Date(Date.UTC(2018, 10, 8, 14, 30, 00));
+var curSe;
+var chSe;
 
 const monthNames = [
   "January",
@@ -32,49 +37,48 @@ function init() {
   chDateText = document.getElementById("chDate");
   curDateText = document.getElementById("curDate");
   fDateText = document.getElementById("fDate");
-
-  curDateText.innerHTML =
-    dayNames[curDate.getDay() - 1] +
-    ", " +
-    curDate.getDate() +
-    " " +
-    monthNames[curDate.getMonth()] +
-    " " +
-    curDate.getFullYear();
-
-  fDateText.innerHTML =
-    dayNames[fDate.getDay() - 1] +
-    ", " +
-    fDate.getDate() +
-    " " +
-    monthNames[fDate.getMonth()] +
-    " " +
-    fDate.getFullYear();
+  chSeText = document.getElementById("chSe");
+  curSeText = document.getElementById("curSe");
 
   UpdateCount();
+
+  SetDate(curDateText, curDate);
+  SetDate(fDateText, fDate);
+
+  var seGone = Math.floor(
+    Math.abs((curDate - origin) / (7 * 24 * 60 * 60 * 1000))
+  );
+
+  chSe = se[(seGone + 1) % 4];
+  curSe = se[seGone % 4];
+
+  fDate = new Date(origin);
+  fDate.setDate(fDate.getDate() + (seGone + 1) * 7);
+
+  chSeText.innerHTML = chSe;
+  curSeText.innerHTML = curSe;
 }
 
-function GetNextDayOfWeek(date, dayOfWeek, hour) {
-  var resultDate = new Date(date.getTime());
-  resultDate.setDate(date.getDate() + ((7 + dayOfWeek - date.getDay()) % 7));
-  resultDate.setHours(hour, 0, 0, 0);
-  return resultDate;
+function SetDate(text, date) {
+  text.innerHTML =
+    dayNames[date.getDay() - 1] +
+    ", " +
+    date.getDate() +
+    " " +
+    monthNames[date.getMonth()] +
+    " " +
+    date.getFullYear();
 }
 
 function UpdateCount() {
   var x = setInterval(function() {
     curDate = new Date();
-    fDate = GetNextDayOfWeek(new Date(), 4, 8);
-    chDate = fDate.getTime();
+    chDate = fDate - curDate;
 
-    var distance = chDate - curDate;
-
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var days = Math.floor(chDate / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((chDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((chDate % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((chDate % (1000 * 60)) / 1000);
 
     chDateText.innerHTML =
       days + "d " + hours + "h " + minutes + "m " + seconds + "s";
