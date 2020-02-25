3b7a803b90545d99d65e0dbdcdd242daa6eccb09
Wouter Koopman <wouter.koopman@student.fontys.nl>
2020-02-25 20:08:15 +0100

    Bug fixes

--- js/dark.js
+++ js/dark.js
@@ -2,7 +2,7 @@ function initDark() {
   const tog = document.querySelector(".toggle");
   const body = document.querySelector("body");
 
-  if (localStorage.getItem("dark") == true) {
+  if (localStorage.getItem("dark") == "true") {
     tog.classList.add("on");
     body.classList.add("dark");
   }
@@ -11,11 +11,11 @@ function initDark() {
     if (tog.classList.contains("on")) {
       tog.classList.remove("on");
       body.classList.remove("dark");
-      localStorage.setItem("dark", false);
+      localStorage.setItem("dark", "false");
     } else {
       tog.classList.add("on");
       body.classList.add("dark");
-      localStorage.setItem("dark", true);
+      localStorage.setItem("dark", "true");
     }
   });
 }
