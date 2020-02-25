0ef954156425481697725e227e7b50059b70d4d4
Wouter Koopman <wouter.koopman@student.fontys.nl>
2020-02-25 13:38:21 +0100

    Added forzathon functionality, updated stylesheet

--- js/live.js
+++ js/live.js
@@ -1,6 +1,50 @@
-function initLive() {
-  const live = document.querySelector(".live");
+var live;
+var liveT;
 
-  live.style.opacity = 0;
-  live.style.pointerEvents = "none";
+var now = new Date();
+var nextH = new Date();
+var nextT;
+
+var liveDelay = 1000;
+
+function initLive() {
+  live = document.querySelector(".live");
+  liveT = document.getElementById("live");
+
+  UpdateText();
+}
+
+function UpdateText() {
+  setTimeout(function() {
+    now = new Date();
+    nextH = new Date();
+    nextH.setHours(nextH.getHours() + 1);
+    nextH.setMinutes(0);
+    nextH.setSeconds(0);
+
+    nextT = nextH.getTime() - now.getTime();
+
+    var minutes = Math.floor((nextT % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((nextT % (1000 * 60)) / 1000);
+
+    if (minutes > 15) {
+      liveDelay = 60000;
+    } else {
+      liveDelay = 1000;
+    }
+    if (minutes == 10 && seconds == 0) {
+      live.classList.remove("hidden");
+    }
+    if (minutes == 0 && seconds == 0) {
+      live.classList.add("hidden");
+    }
+
+    SetText(liveT, minutes, seconds);
+
+    UpdateText();
+  }, liveDelay);
+}
+
+function SetText(text, min, sec) {
+  text.innerHTML = min + ":" + sec;
 }
