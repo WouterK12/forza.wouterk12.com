2c127d89fb727a5b466c683a47aba952ba0668fc
Wouter Koopman <wouter.koopman@student.fontys.nl>
2020-02-27 08:32:16 +0100

    Added notifications toggle

--- js/misc/tog.js
+++ js/misc/tog.js
@@ -1,10 +1,14 @@
 function initTog() {
   const togD = document.querySelector(".toggle.dark");
+  const togN = document.querySelector(".toggle.no");
   const body = document.querySelector("body");
 
   if (localStorage.getItem("dark") == "true") {
     togD.classList.add("on");
     body.classList.add("dark");
+  }
+  if (localStorage.getItem("no") == "true") {
+    togN.classList.add("on");
   }
 
   togD.addEventListener("click", function() {
@@ -16,6 +20,15 @@ function initTog() {
       togD.classList.add("on");
       body.classList.add("dark");
       localStorage.setItem("dark", "true");
+    }
+  });
+  togN.addEventListener("click", function() {
+    if (togN.classList.contains("on")) {
+      togN.classList.remove("on");
+      localStorage.setItem("no", "false");
+    } else {
+      togN.classList.add("on");
+      localStorage.setItem("no", "true");
     }
   });
 }
